73b1c7b19ff60a9db2583885ecbd95d3ee2e5005
Morgan Hallgren <morgan.hallgren@upptec.se>
2014-02-27 13:08:30 +0100

    def interface for aggregate_root

--- lib/sandthorn.rb
+++ lib/sandthorn.rb
@@ -1,4 +1,5 @@
 require "sandthorn/version"
+require "sandthorn/aggregate_root"
 
 module Sandthorn
   # Your code goes here...
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,15 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Bundler.require
+
+module Helpers
+  def class_including(mod)
+    Class.new.tap {|c| c.send :include, mod }
+  end
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true

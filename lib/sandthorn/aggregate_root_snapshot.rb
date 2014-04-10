5db61935daf4de8e7fe3917523da7d94469257f4
Lars Krantz <lars.krantz@alaz.se>
2014-04-10 15:44:28 +0200

    Refactor: cleanup in AggregateRootSnapshot

--- lib/sandthorn/aggregate_root_snapshot.rb
+++ lib/sandthorn/aggregate_root_snapshot.rb
@@ -9,11 +9,7 @@ module Sandthorn
     end
 
     def aggregate_snapshot!
-
-      if @aggregate_events.count > 0
-        raise Errors::SnapshotError.new "Can't take snapshot on object with unsaved events"
-      end
-
+      raise Errors::SnapshotError.new "Can't take snapshot on object with unsaved events" if @aggregate_events.count > 0
       @aggregate_snapshot = {
         :event_name => "aggregate_set_from_snapshot",
         :event_args => [self],
@@ -26,19 +22,6 @@ module Sandthorn
       @aggregate_snapshot[:event_data] = Sandthorn.serialize aggregate_snapshot[:event_args]
       @aggregate_snapshot[:event_args] = nil
       Sandthorn.save_snapshot aggregate_snapshot, aggregate_id, self.class.name
-      @aggregate_snapshot = nil
-    end
-    private
-    def aggregate_create_event_when_extended
-      self.aggregate_snapshot!
-      vars = extract_relevant_aggregate_instance_variables
-      vars.each do |var_name|
-        value = instance_variable_get var_name
-        dump = Marshal.dump(value)
-        store_aggregate_instance_variable var_name, dump
-      end
-      #@aggregate_snapshot[:event_data] = Sandthorn.serialize @aggregate_snapshot[:event_args]
-      store_aggregate_event "instance_extended_as_aggregate", @aggregate_snapshot[:event_args]
       @aggregate_snapshot = nil
     end
   end

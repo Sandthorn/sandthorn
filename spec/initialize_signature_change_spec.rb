728197fbe9d26508bae2d240c6ae9e8fa4e45c67
Lars Krantz <lars.krantz@alaz.se>
2014-10-27 15:06:06 +0100

    Spec: Added failing spec when initialize method signature changes

---
+++ spec/initialize_signature_change_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+class InitChange
+  include Sandthorn::AggregateRoot
+  attr_reader :foo
+  def initialize foo:
+    @foo = foo
+  end
+end
+def change_init
+  InitChange.class_eval do
+    define_method :initialize, lambda { @foo = :foo }
+  end
+end
+describe "when the initialize-method changes" do
+  it "should be possible to replay anyway" do
+    aggregate = InitChange.new foo: :bar
+    events = aggregate.aggregate_events
+    change_init
+    with_change = InitChange.new
+    expect(with_change.foo).to eql :foo
+    replayed = InitChange.aggregate_build(events)
+    expect(replayed.foo).to eql :bar
+  end
+end

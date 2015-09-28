dcb591ccbe5b5e08f3fc795d143ca3f861b36160
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-09-28 15:01:17 +0200

    use m instead of mo

--- lib/sandthorn/bounded_context.rb
+++ lib/sandthorn/bounded_context.rb
@@ -13,11 +13,11 @@ module Sandthorn
         classes = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Class)
         aggregate_list = classes.select { |item| item.include?(Sandthorn::AggregateRoot) }
 
-        modules = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Module).delete_if do |mo| 
-          classes.include?(mo) || mo == Sandthorn::BoundedContext::ClassMethods
+        modules = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Module).delete_if do |m| 
+          classes.include?(m) || m == Sandthorn::BoundedContext::ClassMethods
         end
         
-        aggregate_list += modules.flat_map { |mo| p_aggregate_list(mo) }
+        aggregate_list += modules.flat_map { |m| p_aggregate_list(m) }
         
         aggregate_list
       end

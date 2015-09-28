50aa9d8df46549ddd5c334824d382414307ee5d8
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-09-28 15:38:27 +0200

    extract get_classes and get_modules to private methods

--- lib/sandthorn/bounded_context.rb
+++ lib/sandthorn/bounded_context.rb
@@ -10,16 +10,23 @@ module Sandthorn
       def p_aggregate_list(bounded_context_module)
         return [] unless bounded_context_module.respond_to?(:constants)
         
-        classes = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Class)
+        classes = get_classes(bounded_context_module)
         aggregate_list = classes.select { |item| item.include?(Sandthorn::AggregateRoot) }
-
-        modules = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Module).delete_if do |m| 
-          classes.include?(m) || m == Sandthorn::BoundedContext::ClassMethods
-        end
+        modules = get_modules(bounded_context_module, classes)
         
         aggregate_list += modules.flat_map { |m| p_aggregate_list(m) }
         
         aggregate_list
+      end
+
+      def get_classes namespace
+        namespace.constants.map(&namespace.method(:const_get)).grep(Class)
+      end
+
+      def get_modules namespace, classes
+        namespace.constants.map(&namespace.method(:const_get)).grep(Module).delete_if do |m| 
+          classes.include?(m) || m == Sandthorn::BoundedContext::ClassMethods
+        end
       end
     end
 

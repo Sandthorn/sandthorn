a09d7d40fb35635599832cef62b0f8c68e64cb89
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-09-28 12:29:19 +0200

    refactor

--- lib/sandthorn/bounded_context.rb
+++ lib/sandthorn/bounded_context.rb
@@ -9,22 +9,27 @@ module Sandthorn
       private
       def p_aggregate_list(bounded_context_module)
         return [] unless bounded_context_module.respond_to?(:constants)
+        
         classes = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Class)
         aggregate_list = classes.select { |item| item.include?(Sandthorn::AggregateRoot) }
 
-        modules = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Module).delete_if do |mo| classes.include?(mo) || mo == Sandthorn::BoundedContext::ClassMethods end
+        modules = bounded_context_module.constants.map(&bounded_context_module.method(:const_get)).grep(Module).delete_if do |mo| 
+          classes.include?(mo) || mo == Sandthorn::BoundedContext::ClassMethods
+        end
+        
         modules.each do |mo|
           aggregate_list.concat(p_aggregate_list(mo))
         end
+        
         aggregate_list
       end
     end
 
     extend ClassMethods
+    
     def self.included( other )
       other.extend( ClassMethods )
     end
-    
 
   end
 end

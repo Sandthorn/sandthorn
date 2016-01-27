2a7f76f193092f1b00ca734eeeebd63da4255f9b
Morgan Hallgren <morgan.hallgren@gmail.com>
2016-01-27 22:40:11 +0100

    added more specs

--- spec/default_attributes_spec.rb
+++ spec/default_attributes_spec.rb
@@ -5,20 +5,35 @@ class DefaultAttributes
   def initialize 
   end
 end
-def add_default_attributes
-  DefaultAttributes.class_eval do
-    attr_reader :array
-    define_method :default_attributes, lambda { @array = [] }
-  end
-end
+
+
 describe "when the initialize-method changes" do
-  it "should be possible to replay anyway" do
-    aggregate = DefaultAttributes.new 
-    events = aggregate.aggregate_events
-    add_default_attributes
-    with_change = DefaultAttributes.new
-    expect(with_change.array).to eql []
-    replayed = DefaultAttributes.aggregate_build(events)
-    expect(replayed.array).to eql []
+
+  it "should not have an array attribute on first version of the DefaultAttributes class" do
+    aggregate = DefaultAttributes.new
+    expect(aggregate.respond_to?(:array)).to be_falsy
+  end
+
+  context "default_attributes" do
+
+    def add_default_attributes
+      DefaultAttributes.class_eval do
+        attr_reader :array
+        define_method :default_attributes, lambda { @array = [] }
+      end
+    end
+
+    it "should have set the array attribute to [] on rebuilt " do
+      aggregate = DefaultAttributes.new
+      add_default_attributes
+      rebuilt_aggregate = DefaultAttributes.aggregate_build(aggregate.aggregate_events)
+      expect(rebuilt_aggregate.array).to eql []
+    end
+
+    it "should have an set the array attribute to [] on new" do
+      add_default_attributes
+      aggregate = DefaultAttributes.new
+      expect(aggregate.array).to eql []
+    end
   end
 end

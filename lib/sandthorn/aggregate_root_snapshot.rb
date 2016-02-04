999fa269181559e09774ebf008bcd7fd219ae26f
Björn Skarner <bjorn@skarner.eu>
2016-02-04 20:56:13 +0100

    Remove `aggregate_create_event_when_extended` not used

--- lib/sandthorn/aggregate_root_snapshot.rb
+++ lib/sandthorn/aggregate_root_snapshot.rb
@@ -30,22 +30,5 @@ module Sandthorn
       Sandthorn.save_snapshot(aggregate_snapshot: @aggregate_snapshot, aggregate_id: @aggregate_id, aggregate_type: self.class)
       @aggregate_snapshot = nil
     end
-    private
-    def aggregate_create_event_when_extended
-      self.aggregate_snapshot!
-      vars = extract_relevant_aggregate_instance_variables
-      vars.each do |var_name|
-        value = instance_variable_get var_name
-        dump = Marshal.dump(value)
-        store_aggregate_instance_variable var_name, dump
-      end
-
-      @aggregate_snapshot[:event_data] = Sandthorn
-        .serialize_snapshot aggregate_snapshot[:event_args]
-
-      @aggregate_snapshot[:event_args] = nil
-      Sandthorn.save_snapshot aggregate_snapshot, aggregate_id
-      @aggregate_snapshot = nil
-    end
   end
 end

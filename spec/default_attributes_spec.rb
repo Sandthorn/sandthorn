fd6a10654da6f08ef14fa7cf352deae0c08286ce
Morgan Hallgren <morgan.hallgren@gmail.com>
2016-04-11 09:00:09 +0200

    added spec to make sure the default attribute is not overwritten

--- spec/default_attributes_spec.rb
+++ spec/default_attributes_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
-class DefaultAttributes
-  include Sandthorn::AggregateRoot
-  def initialize 
-  end
-end
+# class DefaultAttributes
+#   include Sandthorn::AggregateRoot
+#   def initialize
+#   end
+# end
 
 
 describe "when the initialize-method changes" do
+
+  before do
+    class DefaultAttributes
+      include Sandthorn::AggregateRoot
+      def initialize
+      end
+    end
+
+  end
+
+  #Make sure the DefaultAttributes class are reset on every test
+  after do
+    Object.send(:remove_const, :DefaultAttributes)
+  end
 
   it "should not have an array attribute on first version of the DefaultAttributes class" do
     aggregate = DefaultAttributes.new
@@ -20,14 +34,11 @@ describe "when the initialize-method changes" do
       DefaultAttributes.class_eval do
         attr_reader :array
         define_method :default_attributes, lambda { @array = [] }
+        define_method :add_item, lambda { |item|
+          @array << item
+          commit
+        }
       end
-    end
-
-    it "should have set the array attribute to [] on rebuilt " do
-      aggregate = DefaultAttributes.new
-      add_default_attributes
-      rebuilt_aggregate = DefaultAttributes.aggregate_build(aggregate.aggregate_events)
-      expect(rebuilt_aggregate.array).to eql []
     end
 
     it "should have an set the array attribute to [] on new" do
@@ -35,5 +46,21 @@ describe "when the initialize-method changes" do
       aggregate = DefaultAttributes.new
       expect(aggregate.array).to eql []
     end
+
+    it "should have set the array attribute to [] on rebuilt when attribute is intruduced after `new`" do
+      aggregate = DefaultAttributes.new
+      add_default_attributes
+      rebuilt_aggregate = DefaultAttributes.aggregate_build(aggregate.aggregate_events)
+      expect(rebuilt_aggregate.array).to eql []
+    end
+
+    it "should set the array attribute to ['banana'] on rebuilt" do
+      add_default_attributes
+      aggregate = DefaultAttributes.new
+      aggregate.add_item 'banana'
+      rebuilt_aggregate = DefaultAttributes.aggregate_build(aggregate.aggregate_events)
+      expect(rebuilt_aggregate.array).to eql ['banana']
+    end
+
   end
 end
